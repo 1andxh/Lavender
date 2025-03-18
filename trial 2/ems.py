93782c7ec72a22b4ea178f8ea99b670d40071b3b
1andxh <solomonkingsman@gmail.com>
2025-03-18 18:03:23 +0000

    employee management system

---
+++ trial 2/ems.py
@@ -0,0 +1,88 @@
+import psycopg2
+
+DB_NAME = "employee_db"
+DB_PASSWORD = "even@gods!"
+DB_HOST = "localhost"
+DB_USER = "postgres"
+DB_PORT = "5432"
+
+try:
+    conn = psycopg2.connect(
+        dbname = DB_NAME,
+        user = DB_USER,
+        password = DB_PASSWORD,
+        host = DB_HOST,
+        port = DB_PORT
+    )
+    print("Connection established")
+except Exception as e:
+    print("Failed to connect to database")
+    print(f"Error: {e}")
+
+cur = conn.cursor()
+
+try: 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS employees(
+        id SERIAL PRIMARY KEY ,
+        name VARCHAR(255) NOT NULL,
+        age INT NOT NULL,
+        department VARCHAR(100) NOT NULL
+    )
+    """)
+except Exception as e:
+    print("Failed to create table")
+    print(f"Error: {e}")
+conn.commit()
+
+name  = input("Enter full name: ")
+age = int(input("Enter age: "))
+department = input("Enter name of department: ")
+
+employee_input = (name,age,department)
+insert_query = ("INSERT INTO employees(name,age,department) VALUES (%s, %s, %s)")
+try:
+    def add_employee():
+        cur.execute(insert_query,employee_input)
+    add_employee()
+    print(f"Employee '{name}' has been added")
+    conn.commit()
+except Exception as e:
+    print(e)
+
+try:
+    def view_employee_info():
+        cur.execute("SELECT * FROM employee")
+        employee_list = cur.fetchall()
+        print("List of all employees\n")
+        for employee in employee_list:
+            print(f"ID: {employee[0]}, Name: {employee[1]}, Age: {employee[2]}, Department: {employee[3]}")
+        view_employee_info()
+except Exception as e:
+    print("Failed to retrieve employee list")
+    print(f"Error: {e}")
+
+
+def update_employee_records():
+    pass
+    # update_employee_records()
+
+
+
+def main():
+    print("=== Employee Management System ===\n")
+    print("Menu\n1. Add Employee\n2. View Employees\3. Update Employee \4. Delete Employee\5. Exit")
+
+    user_input = input("Enter option(1-5): ")
+    if user_input == "1":
+        add_employee()
+    elif user_input == "2":
+        view_employee_info()
+
+
+
+if __name__ == "__main__":
+    main()
+
+cur.close()
+conn.close()
